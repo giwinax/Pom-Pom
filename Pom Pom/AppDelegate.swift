be063ac16c7af573e115ba29d6ad0300f6aa8c35
Stanislav <giwinax@gmail.com>
2022-02-07 19:51:36 +0300

    init commit

--- Pom Pom/AppDelegate.swift
+++ Pom Pom/AppDelegate.swift
@@ -1,30 +1,58 @@
 //
-//  AppDelegate.swift
+//  Pom_PomApp.swift
 //  Pom Pom
 //
 //  Created by s b on 07.02.2022.
 //
 
-import Cocoa
+import SwiftUI
+
+private let nc = NotificationCenter.default
+
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
     
-
-
+    private var statusItem: NSStatusItem!
+    
+    private func createNotification (_ notificationName: String) {
+        var notification = NSUserNotification()
+        notification.title = notificationName
+        //notification.informativeText = "The body of this Swift notification"
+        notification.soundName = NSUserNotificationDefaultSoundName
+        NSUserNotificationCenter.default.deliver(notification)
+    }
+    
+    private func changeStatusBarButton(_ icon:String) {
+        if let button = statusItem.button {
+            button.image = NSImage(systemSymbolName: icon, accessibilityDescription: "1")
+        }
+    }
+    
+    @objc func start() {
+        self.createNotification("25 min to go!")
+        _ = Timer.scheduledTimer(withTimeInterval: 1500.0, repeats: true) { timer in
+            self.createNotification("Time to break!")
+            self.changeStatusBarButton("brain")
+        }
+        _ = Timer.scheduledTimer(withTimeInterval: 300.0, repeats: true) { timer in
+            self.createNotification("Time to break!")
+            self.changeStatusBarButton("figure.walk")
+        }
+        self.changeStatusBarButton("alarm")
+    }
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+        changeStatusBarButton("alarm")
+        
+        let menu = NSMenu()
+        let startMenuItem = NSMenuItem(title: "Start", action: #selector(start) , keyEquivalent: "1")
+        menu.addItem(startMenuItem)
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        statusItem.menu = menu
+        
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-        return true
-    }
-
-
 }
-
